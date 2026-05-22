0a55bb25e86c448d0a4aa868cdcc9b291ae1341d
Bharath Reddy Voodem <voodembharath@gmail.com>
2026-05-22 18:14:23 +0530

    Day 13: Added FastAPI authentication APIs

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from app.auth import router as auth_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "API Working Successfully"}
