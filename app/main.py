2d01df6f015a13adcdb44c68946d93b17d6ec8b4
Bharath Reddy Voodem <voodembharath@gmail.com>
2026-05-25 20:14:57 +0530

    Day 14: Added Streamlit frontend dashboard

--- app/main.py
+++ app/main.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI
-from app.auth import router as auth_router
+from pydantic import BaseModel
+import joblib
+import numpy as np
 
 app = FastAPI()
 
-app.include_router(auth_router)
+# Load model
+model = joblib.load("app/models/logistic_regression_model.pkl")
 
+class CustomerData(BaseModel):
+    tenure: int
+    MonthlyCharges: float
+    TotalCharges: float
 
 @app.get("/")
 def home():
     return {"message": "API Working Successfully"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+@app.post("/predict")
+def predict(data: CustomerData):
+
+    features = np.array([[
+        data.tenure,
+        data.MonthlyCharges,
+        data.TotalCharges
+    ]])
+
+    prediction = model.predict(features)[0]
+
+    return {
+        "prediction": int(prediction)
+    }
